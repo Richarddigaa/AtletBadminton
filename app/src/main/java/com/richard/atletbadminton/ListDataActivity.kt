31f5b8a4e7bd3b1b6ba63a51b2755c0fbc077ab8
Richard Diga Andreansyah <richarddigaa@gmail.com>
2022-07-05 10:30:36 +0700

    Initial commit

---
+++ app/src/main/java/com/richard/atletbadminton/ListDataActivity.kt
@@ -0,0 +1,11 @@
+package com.richard.atletbadminton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_data)
+    }
+}
